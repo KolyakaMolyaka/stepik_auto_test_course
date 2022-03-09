aa6a546d4cd73cfef7882dd5f85bc0159e1c8721
KolyakaMolyaka <zz5dy6@gmail.com>
2022-03-09 16:24:16 +0300

    Добавлено решение 6-го задания из 3-го блока

--- block2/lesson3_step6.py
+++ block2/lesson3_step6.py
@@ -1,10 +1,10 @@
-t from selenium import webdriver
+from selenium import webdriver
 from math import log,sin
 
 def calc(x: str) -> str:
     return str(log(abs(12 * sin(int(x)))))
 
-link = 'http://suninjuly.github.io/alert_accept.html'
+link = 'http://suninjuly.github.io/redirect_accept.html'
 try:
     browser = webdriver.Chrome()
     browser.get(link)
@@ -13,9 +13,9 @@ try:
     button = browser.find_element_by_css_selector('[type="submit"]')
     button.click()
 
-    # accept alert
-    browser.switch_to.alert.accept()
 
+    # switch to the new tab, which was opened after click on button
+    second_window = browser.switch_to.window(browser.window_handles[1])
 
     # Get x and calculate f(x)
     x = browser.find_element_by_id('input_value').text
