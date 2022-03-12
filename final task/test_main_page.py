6d2a41acbbcd0c0dbae988e3fd18a136871c13b2
Nikolay Markeev <zz5dy6@gmail.com>
2022-03-12 16:08:48 +0300

    Добавлен класс LoginPage

--- final task/test_main_page.py
+++ final task/test_main_page.py
@@ -1,10 +1,15 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link) # инициализация Page Object
-    page.open() # открытие страницы
-    page.go_to_login_page() # переход на страницу логина
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page() # открываем страницу авторизации
+
+    login_link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+    page = LoginPage(browser, login_link)
+    page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
