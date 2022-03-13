9ea52b408d213676ade2807bf08ccfc796958f71
Nikolay Markeev <zz5dy6@gmail.com>
2022-03-13 14:35:26 +0300

    Добавлен тест на добавление в корзину со страницы товара

--- final task/pages/locators.py
+++ final task/pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTATION_FROM = (By.CSS_SELECTOR, '#register_form')
+
+class ProductPageLocators():
+    PRODUCT_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
+    PRODUCT_IN_BASKET_PRICE = (By.CSS_SELECTOR, '.alertinner p strong')
+    PRODUCT_TITLE = (By.TAG_NAME, 'h1')
+    PRODUCT_IN_BASKET_TITLE = (By.CSS_SELECTOR, '.alertinner strong') # product should be first with alertinner
---
+++ final task/pages/product_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+
+    def add_product_to_basket(self):
+        add_product_btn = self.browser.find_element(*ProductPageLocators.PRODUCT_BASKET)
+        add_product_btn.click()
+        self.solve_quiz_and_get_code()
+        self.should_be_same_titles()
+        self.should_be_same_prices()
+
+    def should_be_same_titles(self):
+        expected = self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
+        got = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_BASKET_TITLE).text
+        assert expected == got, f'Book title and book in basket title are different: expected {expected}, got {got}'
+
+    def should_be_same_prices(self):
+        expected = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        got = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_BASKET_PRICE).text
+        assert expected == got, f'Book price and book price in basket are different: expected {expected}, got {got}'
+
+    def should_be_add_to_basket_btn(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_BASKET), 'Add product to basket is not presented'
